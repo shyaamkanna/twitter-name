9d2da9f8704adab68ae945fa16c69153de0d5aa3
diazonic <61781809+diazonic@users.noreply.github.com>
2020-08-01 01:48:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,10 +7,10 @@ logger = logging.getLogger()
 
 
 def create_api():
-    CONSUMER_KEY = os.environ('CONSUMER_KEY')
-    CONSUMER_SECRET = os.environ('CONSUMER_SECRET')
-    ACCESS_TOKEN = os.environ('ACCESS_TOKEN')
-    ACCESS_TOKEN_SECRET = os.environ('ACCESS_TOKEN_SECRET')
+    CONSUMER_KEY = os.getenv('CONSUMER_KEY')
+    CONSUMER_SECRET = os.getenv('CONSUMER_SECRET')
+    ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')
+    ACCESS_TOKEN_SECRET = os.getenv('ACCESS_TOKEN_SECRET')
     
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
